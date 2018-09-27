7f30a57b6a8a42041f1150699c4a9e59b6124d3c
hugo valois <hvgaming@yahoo.com>
2018-09-27 21:17:44 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,14 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :author, :page_count, :genre
+  attr_reader :title
+ 
+  def initialize(brand)
+    @brand = brand
+  end
+ 
+  def turn_page
+    puts "Your shoe is as good as new!"
+  end
+ 
+end
